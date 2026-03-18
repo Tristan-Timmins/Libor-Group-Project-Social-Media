e0800efc45b826cccb9e87526a4d3d2d27fa9a8d
(Student  C00308788) Nicholas Mc Donald <C00308788@setu.ie>
2026-03-18 10:55:18 +0000

    Added founder table

--- src/routes/aboutus/+page.svelte
+++ src/routes/aboutus/+page.svelte
@@ -1,3 +1,33 @@
+<style>
+    .item1 {grid-area: header;}
+    .item2 {grid-area: left;}
+    .item3 {grid-area: main;}
+    .item4 {grid-area: right;}
+    
+    .about-container{
+        text-align: center;
+    }
+
+    .grid-container{
+        display: grid;
+        grid-template-areas: 
+            'header header header header header header'
+            'left left main main right right'
+            'left left main main right right';
+        grid-gap: 10px;
+        background-color: #074b80;
+        padding: 10px;
+    }
+
+    .grid-container > div {
+        background-color: rgba(255, 255, 255, 0.8);
+        text-align: center;
+        padding: 50px 0;
+        font-size: 30px;
+    }
+
+</style>
+
 <div class="about-container">
 <section>
     <h2>Welcome to The Founders page...</h2>
@@ -8,10 +38,14 @@
     </p>
 </section>
 
+<div class="grid-container">
+<div class="item1">Meet The Founders!</div>
+<div class="item2">Founder 1</div>
+<div class="item3">Founder 2</div>
+<div class="item4">Founder 3</div>
 </div>
 
-<style>
-    .about-container{
-        text-align: center;
-    }
-</style>
+</div>
+
+
+

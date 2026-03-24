ed9ac9c667c8159847bb8ce537568c1b61a6e5de
(Student  C00308788) Nicholas Mc Donald <C00308788@setu.ie>
2026-03-24 14:55:44 +0000

    Added some more founder info

--- src/routes/aboutus/+page.svelte
+++ src/routes/aboutus/+page.svelte
@@ -1,11 +1,39 @@
+<div class="about-container">
+<section>
+    <h2>Welcome to The Founders page...</h2>
+    <p>
+        We've decided to create a website to surpass all other social platforms. 
+        Tired of feeling like you don't have a cool place for you and your friends to hang out?
+        Well worry no more.
+    </p>
+</section>
+
+<div class="grid-container">
+<div class="item1">Meet The Founders!</div>
+<div class="item2">Founder 1: Beatrice Murphy
+    <p class="item2-text">This founder was the one who pitched the idea in the first place! 
+        Beatrice pioneered the concept of creating a site to surpass all other social sites. 
+        'Blog N' Log' was designed to be the most fun, most entertaining site you and your friends could ever dream of!</p>
+</div>
+<div class="item3">Founder 2: Alex 
+    <p class="item3-text"></p>
+</div>
+<div class="item4">Founder 3</div>
+</div>
+
+</div>
+
 <style>
     .item1 {grid-area: header;}
-    .item2 {grid-area: left;}
-    .item3 {grid-area: main;}
-    .item4 {grid-area: right;}
+    .item2 {grid-area:left;
+        width: 500px;}
+    .item3 {grid-area: main;
+        width: 500px;}
+    .item4 {grid-area: right;
+        width: 500px;}
     
     .about-container{
-        text-align: center;
+        text-align: center;  
     }
 
     .grid-container{
@@ -23,29 +51,14 @@
         background-color: rgba(255, 255, 255, 0.8);
         text-align: center;
         padding: 50px 0;
-        font-size: 30px;
+        font-size: 20px;
+    }
+
+    .item2-text{
+        text-align: left;
+        font-size: 75%;
+        padding: 10px;
     }
 
 </style>
-
-<div class="about-container">
-<section>
-    <h2>Welcome to The Founders page...</h2>
-    <p>
-        We've decided to create a website to surpass all other social platforms. 
-        Tired of feeling like you don't have a cool place for you and your friends to hang out?
-        Well worry no more.
-    </p>
-</section>
-
-<div class="grid-container">
-<div class="item1">Meet The Founders!</div>
-<div class="item2">Founder 1</div>
-<div class="item3">Founder 2</div>
-<div class="item4">Founder 3</div>
-</div>
-
-</div>
-
-
 

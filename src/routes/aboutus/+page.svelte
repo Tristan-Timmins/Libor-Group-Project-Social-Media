ca7200ccc39989bcbc4cd462050ef9531582163c
(Student  C00308788) Nicholas Mc Donald <C00308788@setu.ie>
2026-03-25 10:54:25 +0000

    changed layout of about us

--- src/routes/aboutus/+page.svelte
+++ src/routes/aboutus/+page.svelte
@@ -1,30 +1,31 @@
 <div class="about-container">
 <section>
-    <h2>Welcome to The Founders page...</h2>
+    <div>
+        <h2 class="welcome-banner">Welcome to The Founders page...</h2>
     <p>
         We've decided to create a website to surpass all other social platforms. 
         Tired of feeling like you don't have a cool place for you and your friends to hang out?
         Well worry no more.
     </p>
+    </div>
 </section>
 
 <div class="grid-container">
-<div class="item1">Meet The Founders!</div>
+<div>Meet The Founders!</div>
 <div class="item2">Founder 1: Beatrice Murphy
-    <p class="item2-text">This founder was the one who pitched the idea in the first place! 
-        Beatrice pioneered the concept of creating a site to surpass all other social sites. 
-        'Blog N' Log' was designed to be the most fun, most entertaining site you and your friends could ever dream of!</p>
+    
 </div>
-<div class="item3">Founder 2: Alex 
-    <p class="item3-text"></p>
+<div class="item3">Founder 2: Alex Humphrey
+    
 </div>
-<div class="item4">Founder 3</div>
+<div class="item4">Founder 3: Jimmy Jackson
+
+</div>
 </div>
 
 </div>
 
 <style>
-    .item1 {grid-area: header;}
     .item2 {grid-area:left;
         width: 500px;}
     .item3 {grid-area: main;
@@ -33,18 +34,22 @@
         width: 500px;}
     
     .about-container{
-        text-align: center;  
+        text-align: center;
+        max-width: 900px;
+        margin: 0 auto;
+        background-color: #ffffff;
+        border-left: 1px solid #c0c0c0;
+        border-right: 1px solid #c0c0c0;
+        border-bottom: 170px solid #ffffff;
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+        gap: 15px;
+        padding: 15px; 
     }
 
     .grid-container{
-        display: grid;
-        grid-template-areas: 
-            'header header header header header header'
-            'left left main main right right'
-            'left left main main right right';
-        grid-gap: 10px;
         background-color: #074b80;
-        padding: 10px;
+        padding: 5px;
     }
 
     .grid-container > div {
@@ -54,11 +59,8 @@
         font-size: 20px;
     }
 
-    .item2-text{
-        text-align: left;
-        font-size: 75%;
-        padding: 10px;
+    .welcome-banner{
+        font-family: 'Comic Sans MS';
     }
-
 </style>
 

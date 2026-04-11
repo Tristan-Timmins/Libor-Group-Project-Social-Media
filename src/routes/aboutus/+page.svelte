ad2b3be0ea4de2dabbbd04bdb5130c634f3919e5
Nicholas McDonald <c00308788@setu.ie>
2026-04-11 18:44:54 +0100

    Founders page improvements

--- src/routes/aboutus/+page.svelte
+++ src/routes/aboutus/+page.svelte
@@ -1,5 +1,6 @@
 <div class="about-container">
 <section>
+<div>
     <div>
         <h2 class="welcome-banner">Welcome to The Founders Page!</h2>
     <p class="welcome-paragraph">
@@ -9,8 +10,11 @@
     </p>
     </div>
 </section>
+<div>
+    <h2 class="founders">Meet the Founders!</h2>
+</div>
+
 <div class="info-section">
-    <h2>Meet the Founders:</h2>
     <div class="beatrice">Founder 1: Beatrice Murphy
         <p class="beatrice-text">
             Beatrice was born in the suburbs as an only child. Since she was little, she dreamed of 
@@ -31,12 +35,20 @@
         <p class="jimmy-text">
             Oh Jimmy, god where do I even begin? I get that he's a trusted friend of Beatrice's but 
             seriously? Jimmy..grew up in a broken home, and used football as an escape from the hurt
-            the worlde dealt him when he was little. He agreed to help if he could use it to start 
+            the world dealt him when he was little. He agreed to help if he could use it to start 
             a football forum within the site. We reluctantly agreed..
         </p>
     </div>
 
 </div>
+
+<h2 class="closing-text">Now that you've met The Founders, support their noble cause and join us today! 
+    Blog N' Log was designed by folks like you, for you!! You'll never want to leave!!
+
+    <p class="closing-pg">Sign up now and create an account to get chatting with your friends. 
+        What are you waiting for? Do it now!
+    You don't want our idea to fail, do you?</p>
+</h2>
 
 </div>
 
@@ -55,7 +67,7 @@
         background-color: #ffffff;
         border-left: 1px solid #c0c0c0;
         border-right: 1px solid #c0c0c0;
-        border-bottom: 450px solid #ffffff;
+        border-bottom: 200px solid #ffffff;
         display: grid;
         grid-template-columns: repeat(1, 1fr);
         gap: 15px;
@@ -67,23 +79,32 @@
         text-align: center;
         margin: 0;
         padding: 10px;
-        border: 5px solid #b4e3fc;
+        border: 5px solid #646e96;
         border-radius: 5px;    
         box-sizing: border-box;
+        background-color: #b4e3fc;
+        text-shadow: 1px 2px rgb(163, 163, 255);
     }
 
     .welcome-paragraph{
         font-family: 'Comic Sans MS';
-        color: #0b0b0b;
+        color: #571b1b;
         padding: 10px;
     }
     /*FOUNDER INFORMATION SECTION**/
     .info-section{
         font-family: 'Comic Sans MS';
         display: grid;
-        grid-template-columns: repeat(4, 1fr);
+        grid-template-columns: repeat(3, 1fr);
         border: 5px solid #646e96;
         background-color: #b4e3fc;
+    }
+
+    .founders{
+        font-family: 'Courier New', Courier, monospace;
+        border: 2px solid #ff8851;
+        background-color: #fcddb9;
+        text-shadow: 1px 1px rgb(169, 109, 59);
     }
     /*FOUNDER 1**/
     .beatrice{
@@ -91,10 +112,13 @@
         font-weight: bolder;
         border: 2px solid #646e96;
         border-left: 5px solid #646e96;
+        padding: 5px;
     }
     .beatrice-text{
         font-family: 'Courier New', Courier, monospace;
         font-weight: bold;
+        font-size: small;
+        padding: 10px;
     }
     
     /*FOUNDER 2**/
@@ -102,10 +126,13 @@
         font-family: 'Courier New', Courier, monospace;
         font-weight: bolder;
         border: 2px solid #646e96;
+        padding: 5px;
     }
     .alex-text{
         font-family: 'Courier New', Courier, monospace;
         font-weight: bold;
+        font-size: small;
+        padding: 10px;
     }
     
     /*FOUNDER 3**/
@@ -113,10 +140,26 @@
         font-family: 'Courier New', Courier, monospace;
         font-weight: bolder;
         border: 2px solid #646e96;
+        padding: 5px;
     }
     .jimmy-text{
         font-family: 'Courier New', Courier, monospace;
         font-weight: bold;
+        font-size: small;
+        padding: 10px;
+    }
+    /*CLOSING MESSAGE**/
+    .closing-text{
+        font-family: 'Comic Sans MS';
+        padding: 10px;
+        font-size: medium;
+        border: 2px solid #e4c632;
+        background-color: #fefcc3;
+    }
+    .closing-pg{
+        font-family: 'Courier New', Courier, monospace;
+        padding: 10px;
+        font-size: small;
     }
 </style>
 
